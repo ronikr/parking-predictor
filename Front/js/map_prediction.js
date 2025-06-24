3d964deef8857c7f8d77dff63e6ff3b6c497077f
ronikr <roooonik@gmail.com>
2025-06-24 17:58:56 +0300

    added tgl to prediction map

--- Front/js/map_prediction.js
+++ Front/js/map_prediction.js
@@ -6,6 +6,20 @@ const weekdays = [
   "ראשון", "שני", "שלישי", "רביעי", "חמישי", "שישי", "שבת"
 ];
 
+const holidays = [
+  "ערב פסח",
+  "פסח א׳",
+  "ערב חג שני",
+  "פסח 7",
+  "ערב יום השואה",
+  "יום השואה",
+  "ערב יום הזיכרון",
+  "יום הזיכרון",
+  "יום העצמאות",
+  "ערב שבועות",
+  "שבועות"
+];
+
 const statusColors = {
   available: "green",
   limited: "orange",
@@ -25,9 +39,14 @@ const statusLabels = {
 const daySelect = document.getElementById("daySelect");
 const hourSelect = document.getElementById("hourSelect");
 const loadButton = document.getElementById("loadButton");
+const holidayToggle = document.getElementById("holidayToggle");
+holidayToggle.addEventListener("change", populateDays);
+
 
 function populateDays() {
-  daySelect.innerHTML = weekdays.map(day => `<option value="${day}">${day}</option>`).join("");
+  const useHolidays = holidayToggle.checked;
+  const options = useHolidays ? holidays : weekdays;
+  daySelect.innerHTML = options.map(day => `<option value="${day}">${day}</option>`).join("");
 }
 
 populateDays();
@@ -78,3 +97,11 @@ async function loadMapData() {
     console.error(err);
   }
 }  
+
+const statusMap = {
+        panui: "available",
+        pail: "unknown",
+        meat: "limited",
+        male: "full",
+        // ...all other results, including null :"no_data"
+    };
